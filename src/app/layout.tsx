21ed657ed740d448f8e79724f7bdff0d0d7cca39
Muhammad Hamza <106156324+Hamzaansari15@users.noreply.github.com>
2025-08-17 18:29:00 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,15 +6,18 @@ import Footer from "@/components/footer/page";
 
 const glory = Glory({
   variable: "--font-glory",
+  subsets: ["latin"],
 });
 
 const poppins = Poppins({
   variable: "--font-poppins",
+  subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 const roboto = Roboto({
   variable: "--font-roboto",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
